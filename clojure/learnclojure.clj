73b9b90d7765614c40955380129101aae394601b
Deoren Moor <deoren@users.noreply.github.com>
2018-05-18 15:18:12 -0500

    Update learnclojure.clj

--- clojure/learnclojure.clj
+++ clojure/learnclojure.clj
@@ -1,5 +1,56 @@
 ;; LightTable 0.8.1: http://lighttable.com/, https://github.com/LightTable/LightTable
 ;; Unofficial "LightTable Square Fix" https://github.com/ArchI3Chris/LightTable-Square-Fix
+;;
 ;; Ubuntu 18.04
 ;; OpenJDK 8 (https://codeyarns.com/2015/12/01/how-to-set-default-java-version-in-ubuntu/)
+;;
+;; https://www.safaribooksonline.com/library/view/learning-clojure/9781771373623/
+
+;; Scratch notes from going through the Learning Clojure video series available
+;; on safaribooksonline.com
+
+(ns learnclojure)
+
+(def x "Hello Clojure")
+
+(let [x "Steve"]
+  (print "Hello, " x))
+
+
+(print x)
+
+(str x)
+
+(if (empty? x)
+  "X is empty"
+  "X is not empty")
+
+
+(if nil
+  "Yes" "No")
+
+(if (empty? x)
+  nil
+    (do
+      (println "Ok")
+      :ok))
+
+
+(let [x ""]
+  (if (empty? x)
+    (do
+      (println "Nope")
+    )
+      (do
+        (println "Ok")
+        :ok)
+    )
+  )
+
+
+(if-not (empty? x)
+  (do
+    (println "Ok")
+    :ok))
+
 
