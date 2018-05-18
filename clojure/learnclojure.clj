9f51c0b5295cf4e8175bed78cdfa7073b854d94f
Deoren Moor <deoren@users.noreply.github.com>
2018-05-18 15:16:46 -0500

    Create learnclojure.clj
    Stub file

---
+++ clojure/learnclojure.clj
@@ -0,0 +1,5 @@
+;; LightTable 0.8.1: http://lighttable.com/, https://github.com/LightTable/LightTable
+;; Unofficial "LightTable Square Fix" https://github.com/ArchI3Chris/LightTable-Square-Fix
+;; Ubuntu 18.04
+;; OpenJDK 8 (https://codeyarns.com/2015/12/01/how-to-set-default-java-version-in-ubuntu/)
+
